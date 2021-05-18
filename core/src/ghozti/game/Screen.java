eb8963d1351985661b953dc769875e61a9f327a9
Ghozti <ybalderas1005@gmail.com>
2021-05-18 13:57:01 -0400

    - added Screen and GraphicsCard

---
+++ core/src/ghozti/game/Screen.java
@@ -0,0 +1,62 @@
+package ghozti.game;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Screen implements com.badlogic.gdx.Screen {
+
+    //screen
+    Camera camera;
+    Viewport viewport;
+    //textures
+    TextureAtlas atlas;
+    SpriteBatch batch;
+    //timing
+    //World
+    final float WORLD_HEIGHT = 1080;
+    final float WORLD_WIDTH = 1920;
+    //objects
+
+
+    public Screen(){
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
