94235143de6e923967fe0e92bc850bd4bdedd7a2
Ghozti <ybalderas1005@gmail.com>
2021-05-20 13:02:26 -0400

    - filled in Scalper

--- core/src/ghozti/game/entities/npc/Scalper.java
+++ core/src/ghozti/game/entities/npc/Scalper.java
@@ -1,4 +1,22 @@
 package ghozti.game.entities.npc;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+
 public class Scalper {
+
+    //attributes
+
+    //speed
+    float speed;
+    //texture
+    TextureRegion texture;
+    //boundingRect
+    com.badlogic.gdx.math.Rectangle boundingRect;
+
+    public Scalper(float speed, TextureRegion region,float x, float y,float width, float height){
+        this.speed = speed;
+        texture = region;
+        boundingRect = new Rectangle(x,y,width,height);
+    }
 }
