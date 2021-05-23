889811167db547dda5ad9e9f2b8f102c79617493
Ghozti <ybalderas1005@gmail.com>
2021-05-23 12:00:14 -0400

    - filled in a bit for Hud

--- core/src/ghozti/game/entities/player/hud/Hud.java
+++ core/src/ghozti/game/entities/player/hud/Hud.java
@@ -1,4 +1,21 @@
 package ghozti.game.entities.player.hud;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
 public class Hud {
+
+    public Hud(){
+        //creates a bitmapFont from our file
+        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("EdgeOfTheGalaxyRegular-OVEa6.otf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        fontParameter.size = 72;
+        fontParameter.borderWidth = 3.6f;
+        fontParameter.color = new Color(1,1,1,0.3f);
+        fontParameter.borderColor = new Color(1,1,1,0.3f);
+
+        font
+    }
 }
