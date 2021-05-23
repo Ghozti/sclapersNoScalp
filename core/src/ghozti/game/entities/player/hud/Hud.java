1371cca340f310e48b6de46bed9cf993fa1b8e4d
Ghozti <ybalderas1005@gmail.com>
2021-05-23 12:23:13 -0400

    - filled in Hud constructor - added a font

--- core/src/ghozti/game/entities/player/hud/Hud.java
+++ core/src/ghozti/game/entities/player/hud/Hud.java
@@ -2,20 +2,40 @@ package ghozti.game.entities.player.hud;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import ghozti.game.screen.Screen;
 
 public class Hud {
 
+    float score,time;
+    BitmapFont font;
+    float verticalMargin,leftx,rightx,centerx,row1Y,row2Y,sectionWidth;
+
     public Hud(){
         //creates a bitmapFont from our file
-        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("EdgeOfTheGalaxyRegular-OVEa6.otf"));
+        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("NugoSansLight-9YzoK.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
 
+        //sets font stuff
         fontParameter.size = 72;
         fontParameter.borderWidth = 3.6f;
         fontParameter.color = new Color(1,1,1,0.3f);
-        fontParameter.borderColor = new Color(1,1,1,0.3f);
+        fontParameter.borderColor = new Color(0,0,0,0.3f);
 
-        font
+        font = fontGenerator.generateFont(fontParameter);
+
+        //sets scale of font
+        font.getData().setScale(0.08f);
+
+        //calculates hud margins,etc
+        verticalMargin = font.getCapHeight()/2;
+        leftx = verticalMargin;
+        rightx = Screen.WORLD_WIDTH * 2 / 3 - leftx;
+        centerx = Screen.WORLD_WIDTH/3;
+        row1Y = Screen.WORLD_HEIGHT - verticalMargin;
+        row2Y = row1Y - verticalMargin - font.getCapHeight();
+        sectionWidth = Screen.WORLD_WIDTH/3;
+
     }
 }
