1bc682293542579a44e50887b6e5fd921c5f5b6d
Ghozti <ybalderas1005@gmail.com>
2021-05-24 15:50:13 -0400

    - added getters for PowerUp

--- core/src/ghozti/game/additionals/PowerUp.java
+++ core/src/ghozti/game/additionals/PowerUp.java
@@ -17,7 +17,11 @@ public abstract class PowerUp {
         boundingRect = new Rectangle(100,100,width,height);
     }
 
-
+    public float getWidth(){return width;};
+    public float getHeight(){return height;};
+    public float getX(){return x;}
+    public float getY() {return y;}
+    public Rectangle getBoundingRect(){return boundingRect;}
 
     public abstract void renderPowerUP(Batch batch);
 }
--- core/src/ghozti/game/additionals/SpeedBoost.java
+++ core/src/ghozti/game/additionals/SpeedBoost.java
@@ -1,4 +1,17 @@
 package ghozti.game.additionals;
 
-public class SpeedBoost {
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+
+public class SpeedBoost extends PowerUp{
+
+    public SpeedBoost(TextureRegion texture, float width, float height) {
+        super(texture, width, height);
+    }
+
+    @Override
+    public void renderPowerUP(Batch batch) {
+
+    }
 }
