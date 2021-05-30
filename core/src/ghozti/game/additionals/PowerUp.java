3afd5c359d7f3b86e239a3be2e4b2106bbdbbf74
Ghozti <ybalderas1005@gmail.com>
2021-05-30 10:45:15 -0400

    - commented on PowerUp

--- core/src/ghozti/game/additionals/PowerUp.java
+++ core/src/ghozti/game/additionals/PowerUp.java
@@ -16,34 +16,46 @@ public abstract class PowerUp {
     TextureRegion texture;
     Rectangle boundingRect;
 
+    //for when the powerup has reached it's end this boolean will reset it
     public boolean spawnNew;
+    //if the powerup has detected collision
     boolean activated;
     //boolean effectApplied;
+    //if the power up has detected collision it will not draw
     boolean hide;
 
     public PowerUp(float width, float height){
+        //sets the position based on a randomizer.
         float[] coordinates = determinePos(Screen.WORLD_WIDTH,Screen.WORLD_HEIGHT);
+        //sets the boundingrect for collision and positioning
         boundingRect = new Rectangle(coordinates[0],coordinates[1],width,height);
     }
 
+    //getters and setters
     public float getWidth(){return boundingRect.width;};
     public float getHeight(){return boundingRect.height;};
     public float getX(){return boundingRect.x;}
     public float getY() {return boundingRect.y;}
     public Rectangle getBoundingRect(){return boundingRect;}
 
+    //resets all booleans and coordinates for when the power up has reached it's end cycle.
     public void reset(){
+        //creates new coordinates
         float[] coordinates = determinePos(Screen.WORLD_WIDTH,Screen.WORLD_HEIGHT);
+        //sets the coordinates to the rect
         boundingRect.setPosition(coordinates[0],coordinates[1]);
+        //resets booleans
         spawnNew = false;
         activated = false;
         //effectApplied = false;
         hide = false;
+        //resets counter
         counter = 0;
         counter2 = 0;
     }
 
     public float[] determinePos(float worldWidth,float worldHeight){
+        //creates 2 floats based on a random number generator
         return new float[]{(float) ((Math.random() * ((worldWidth - 60) - 60)) + 60), (float) ((Math.random() * ((worldHeight - 60) - 60)) + 60)};
     }
 
@@ -66,6 +78,7 @@ public abstract class PowerUp {
     private float counter2;
 
     public void startSpawnerTimer(float delta){
+        //a timer for 10 seconds (the time it takes to spawn a new powerup after the last one was touched)
         if(activated) {
             counter2 += delta;
             if (counter2 > 10.0) {
@@ -78,6 +91,7 @@ public abstract class PowerUp {
     private float counter;
 
     public void startEffectTimer(float delta,Scalper scalper, Player player){
+        //a timer for 5 seconds (the time the powerup effect lasts)
         if(activated) {
             counter += delta;
             if (counter > 5.0) {
