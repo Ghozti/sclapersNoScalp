d3e141d764cf90965bf23befd69293b3bc418440
Ghozti <ybalderas1005@gmail.com>
2021-05-29 22:29:09 -0400

    - commented in GraphicsCard

--- core/src/ghozti/game/entities/graphicsCard/GraphicsCard.java
+++ core/src/ghozti/game/entities/graphicsCard/GraphicsCard.java
@@ -20,10 +20,14 @@ public class GraphicsCard {
     Rectangle boundingRect;
 
     public GraphicsCard(TextureAtlas textureAtlas, float width, float height, float worldW, float worldH){
+        //sets the texture atlas
         this.atlas = textureAtlas;
+        //sets the positions
         float[] positions = setPosition(worldW,worldH);
+        //sets the boundingrect for collision and positioning.
         boundingRect = new Rectangle(positions[0],positions[1],width,height);
 
+        //adds all possible textures into the array
         textureRegions = new TextureRegion[8];
 
         textureRegions[0] = atlas.findRegion("3070");
@@ -35,14 +39,17 @@ public class GraphicsCard {
         textureRegions[6] = atlas.findRegion("6800");
         textureRegions[7] = atlas.findRegion("black6800xt");
 
+        //sets the actual current texture randomly.
         currentRegion = textureRegions[(int) ((Math.random() * ((textureRegions.length - 0)) + 0))];
     }
 
+    //getters and setters
     public float getHeight(){return boundingRect.height;}
     public float getWidth() {return boundingRect.width;}
     public float getX() {return boundingRect.x;}
     public float getY(){return boundingRect.y;}
 
+    //collision detection for scalper and player
     public boolean collides(Player player, Scalper scalper){
         //sets scores for both scalper and player when collides
         if(player.getBoundingRect().overlaps(boundingRect)) player.setScore(player.getScore()+1);
@@ -50,6 +57,7 @@ public class GraphicsCard {
         return player.getBoundingRect().overlaps(boundingRect) || scalper.getBoundingRect().overlaps(boundingRect);
     }
 
+    //creates an array with 2 randomly created floats for the x and y pos
     public float[] setPosition(float worldWidth,float worldHeight){
         float x = (float) ((Math.random() * ((worldWidth - 60) - 60)) + 60);
         float y = (float) ((Math.random() * ((worldHeight - 60) - 60)) + 60);
