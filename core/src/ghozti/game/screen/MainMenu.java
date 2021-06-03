b3c1e501e981ecba7d3763b4a249087114c27dcb
Ghozti <ybalderas1005@gmail.com>
2021-06-03 15:55:42 -0400

    - commented in MainMenu

--- core/src/ghozti/game/screen/MainMenu.java
+++ core/src/ghozti/game/screen/MainMenu.java
@@ -35,28 +35,36 @@ public class MainMenu implements Screen {
     public static final float WORLD_WIDTH = 1920;
 
     //music
-    Music music;
-    Music pauseMusic;
+    Music music;//current background music
+    Music pauseMusic;//sound effect when the player clicks the mute button/start button
     boolean musicOn = true;
 
     public MainMenu() {
 
+        //sets the background music
         music = Gdx.audio.newMusic(Gdx.files.internal("2019-01-02_-_8_Bit_Menu_-_David_Renda_-_FesliyanStudios.com.wav"));
         music.setVolume(0.5f);
         music.setLooping(true);
         music.play();
 
+        //sets the sound effects
         pauseMusic = Gdx.audio.newMusic(Gdx.files.internal("mixkit-quick-jump-arcade-game-239.wav"));
         pauseMusic.setVolume(.5f);
 
+        //sets the background texture
         background = new Texture("menubg.png");
 
+        //sets the start button texture
         startBtn =  new Texture("start.png");
+        //sets the start button when it's activated
         startBtnActive = new Texture("start2.png");
 
+        //sets the music on texture
         musicOnT = new Texture("musicOn.png");
+        //sets the music-off texture
         musicOffT = new Texture("musicOff.png");
 
+        //the current music texture is on by default
         currentMusic = musicOnT;
 
         //sets camera, viewport
@@ -68,9 +76,13 @@ public class MainMenu implements Screen {
     }
 
     public void update(){
+        //updates the screen
+
+        //booleans for if something is on/off
         boolean check1 = false,check2 = false;
         boolean musicCheck1 = false,musicCheck2 = false;
 
+        //the mouse must be positioned in a certain coordinate range for both check1 and check2 to be true
         if (Gdx.input.getX() <= 1162 && Gdx.input.getX() >= 755) {
             check1 = true;
         }
@@ -78,17 +90,20 @@ public class MainMenu implements Screen {
            check2 = true;
         }
 
+        //if check1 and check2 are true the music will stop and the sound effect will play the static "startgame" boolean will be true (this is the start game functionality)
         if(check1 && check2) {
             if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
                 music.stop();
                 pauseMusic.play();
                 startGame = true;
             }
+            //if the mouse hovers over the start game texture it will change it's texture into the blue or "active" button
             currentButton = startBtnActive;
         } else {
             currentButton = startBtn;
         }
 
+        //same functionality as before except it is for the mute functionilty
         if (Gdx.input.getX() >= 1800 && Gdx.input.getX() <= 1880) {
             musicCheck1 = true;
         }
@@ -119,6 +134,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
+        //once the game starts nothing will be rendered
         if (!startGame) {
             Gdx.gl.glClearColor(.128f, .128f, .128f, .1f);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
