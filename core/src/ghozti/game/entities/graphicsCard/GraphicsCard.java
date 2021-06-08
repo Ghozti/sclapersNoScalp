f9eff370d13572b66c8632bc4419c0ec938911ad
Ghozti <ybalderas1005@gmail.com>
2021-06-08 14:12:07 -0400

    - instanciated the font in GraphicsCard

--- core/src/ghozti/game/entities/graphicsCard/GraphicsCard.java
+++ core/src/ghozti/game/entities/graphicsCard/GraphicsCard.java
@@ -2,13 +2,18 @@ package ghozti.game.entities.graphicsCard;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Rectangle;
 import ghozti.game.entities.npc.Scalper;
 import ghozti.game.entities.player.Player;
+import ghozti.game.screen.Screen;
+
 import java.util.ArrayList;
 
 public class GraphicsCard {
@@ -25,6 +30,8 @@ public class GraphicsCard {
     int textureW = 100,textureH = 100;
     //dialogue
     String card;
+    BitmapFont font;
+    float verticalMargin,leftx,rightx,centerx,row1Y,row2Y,sectionWidth;
 
     public GraphicsCard(TextureAtlas textureAtlas, float width, float height, float worldW, float worldH){
         //sets the texture atlas
@@ -77,6 +84,30 @@ public class GraphicsCard {
         }else if (textureRegions[9].equals(currentRegion)) {
             card = "3060ti";
         }
+
+        //creates a bitmapFont from our file
+        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("NugoSansLight-9YzoK.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        //sets font attributes
+        fontParameter.size = 172;
+        fontParameter.borderWidth = 3.6f;
+        fontParameter.color = new Color(1,1,1,.3f);
+        fontParameter.borderColor = new Color(1,1,1,.3f);
+
+        font = fontGenerator.generateFont(fontParameter);
+
+        //sets scale of font
+        font.getData().setScale(.3f);
+
+        //calculates hud margins,etc
+        verticalMargin = font.getCapHeight()/2;
+        leftx = verticalMargin;
+        rightx = Screen.WORLD_WIDTH * 2 / 3 - leftx;
+        centerx = Screen.WORLD_WIDTH/3;
+        row1Y = Screen.WORLD_HEIGHT - verticalMargin;
+        row2Y = row1Y - verticalMargin - font.getCapHeight();
+        sectionWidth = Screen.WORLD_WIDTH/3;
     }
 
     //getters and setters
