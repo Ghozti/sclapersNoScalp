0c06d71373b21daf7527e82058174b7b78a91429
Ghozti <ybalderas1005@gmail.com>
2021-06-09 15:32:45 -0400

    - filled in CharacterSelector

--- core/src/ghozti/game/screen/CharacterSelector.java
+++ core/src/ghozti/game/screen/CharacterSelector.java
@@ -1,4 +1,25 @@
 package ghozti.game.screen;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
+
 public class CharacterSelector {
+
+    String textureID;
+    String characterSelected;
+
+    public CharacterSelector(){
+        textureID = "1";
+    }
+
+    public void handleInput(){
+
+    }
+
+    public String getTextureId(){
+        return textureID;
+    }
+
+    public void draw(Batch batch){
+
+    }
 }
